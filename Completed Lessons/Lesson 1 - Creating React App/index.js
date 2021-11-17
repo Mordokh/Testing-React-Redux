be308511a5d84a0d450e3a069a4005fdce6d6114
Ajdin <ajdinh1997@gmail.com>
2021-11-17 23:33:48 +0100

    Lesson 2 completed

--- Completed Lessons/Lesson 1 - Creating React App/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const App = function(){
-    return <div>Hi there!</div>
-};
-
-ReactDOM.render(<App />, document.querySelector("#root"));
